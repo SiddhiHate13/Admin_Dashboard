0c090d7138af486a2ad40b573ab02ea00fd17a46
SiddhiHate13 <siddhihaate@gmail.com>
2024-07-07 03:14:33 +0530

    CSS update

--- components/SideBar.tsx
+++ components/SideBar.tsx
@@ -9,7 +9,7 @@ const Sidebar: React.FC = () => {
     <div className="bg-[#FFFFFF] w-64 fixed left-0 top-[70px] flex flex-col h-full">
       {/* Button Container */}
       <div className="flex flex-1 flex-col items-center">
-        <button className='bg-[#ffcc66] text-black hover:bg-slate-600 hover:text-white rounded-lg shadow-sm shadow-gray-400 my-14 w-[200px] h-[50px] flex items-center justify-center py-3 font-bold text-xl'>
+        <button className='bg-[#ffcc66] text-black hover:bg-slate-600 hover:text-white rounded-lg shadow-sm shadow-gray-400 mt-16 my-8 w-[200px] h-[50px] flex items-center justify-center py-3 font-bold text-xl'>
           <BsStack className='mr-3' size={22}/>
          Overview
         </button>
